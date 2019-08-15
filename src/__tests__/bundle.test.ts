6cbebc1e3b19d8f1f92057b99f70522b95e7ae0c
keroxp <kerokerokerop@gmail.com>
2019-08-15 21:56:09 +0900

    Update bundle.test.ts

--- src/__tests__/bundle.test.ts
+++ src/__tests__/bundle.test.ts
@@ -4,36 +4,36 @@ describe("bundle", () => {
   describe("normalizeModule", () => {
     test("any + url", () => {
       const res = joinModuleId({
-        canonicalParentName: "https://deno.land/hoge.ts",
-        canonicalName: "https://deno.land/some.ts"
+        moduleId: "https://deno.land/hoge.ts",
+        dependency: "https://deno.land/some.ts"
       });
       expect(res).toBe("https://deno.land/some.ts");
     });
     test("url + relative", () => {
       const res = joinModuleId({
-        canonicalParentName: "https://deno.land/hoge.ts",
-        canonicalName: "./some.ts"
+        moduleId: "https://deno.land/hoge.ts",
+        dependency: "./some.ts"
       });
       expect(res).toBe("https://deno.land/some.ts");
     });
     test("relative + relative", () => {
       const res = joinModuleId({
-        canonicalParentName: "./example/hoge.ts",
-        canonicalName: "./some.ts"
+        moduleId: "./example/hoge.ts",
+        dependency: "./some.ts"
       });
       expect(res).toBe("./example/some.ts");
     });
     test("relative + relative (subdir)", () => {
       const res = joinModuleId({
-        canonicalParentName: "./example/subdir/hoge.ts",
-        canonicalName: "../otherdir/some.ts"
+        moduleId: "./example/subdir/hoge.ts",
+        dependency: "../otherdir/some.ts"
       });
       expect(res).toBe("./example/otherdir/some.ts");
     });
     test("root", () => {
       const res = joinModuleId({
-        canonicalParentName: ".",
-        canonicalName: "./example/some.ts"
+        moduleId: ".",
+        dependency: "./example/some.ts"
       });
       expect(res).toBe("./example/some.ts");
     });
