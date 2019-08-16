8816f52aa8824c70fb6840cf9f1dee7951c54762
keroxp <kerokerokerop@gmail.com>
2019-08-17 03:09:37 +0900

    ver: v0.4.1

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,12 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 // Copyright 2019 Yusuke Sakurai. All rights reserved. MIT license.
 const path = require("path");
@@ -18,124 +10,115 @@ const cacheDir = require("cachedir");
 const fetch_1 = require("./fetch");
 exports.kUriRegex = /^(https?):\/\/(.+?)$/;
 exports.kRelativeRegex = /^\.\.?\/.+?\.ts$/;
-function readFileAsync(file) {
-    return __awaiter(this, void 0, void 0, function* () {
-        return String(yield fs.readFile(file));
-    });
+async function readFileAsync(file) {
+    return String(await fs.readFile(file));
 }
-function fileExists(p) {
-    return __awaiter(this, void 0, void 0, function* () {
-        return fs.pathExists(p);
-    });
+async function fileExists(p) {
+    return fs.pathExists(p);
 }
-function resolveUri(id) {
-    return __awaiter(this, void 0, void 0, function* () {
-        let m;
-        if ((m = id.match(exports.kUriRegex))) {
-            const [_, scheme, pathname] = m;
-            return path.resolve(path.join(cacheDir("deno"), `deps/${scheme}/${pathname}`));
-        }
-        else if (id.match(exports.kRelativeRegex)) {
-            return path.resolve(id);
-        }
-        else {
-            throw new Error("invalid module specifier: " + id);
-        }
-    });
+async function resolveUri(id) {
+    let m;
+    if ((m = id.match(exports.kUriRegex))) {
+        const [_, scheme, pathname] = m;
+        return path.resolve(path.join(cacheDir("deno"), `deps/${scheme}/${pathname}`));
+    }
+    else if (id.match(exports.kRelativeRegex)) {
+        return path.resolve(id);
+    }
+    else {
+        throw new Error("invalid module specifier: " + id);
+    }
 }
-function resolveModuleId(source, skipFetch = false) {
-    return __awaiter(this, void 0, void 0, function* () {
-        let m;
-        if ((m = source.dependency.match(exports.kUriRegex))) {
-            // any + url
-            const [_, scheme, pathname] = m;
-            const cachePath = path.join(cacheDir("deno"), `deps/${scheme}/${pathname}`);
-            if (!(yield fileExists(cachePath))) {
-                if (!(yield fileExists(cachePath + ".headers.json"))) {
-                    if (!skipFetch) {
-                        yield fetch_1.fetchModule(source.dependency, cacheDir("deno"));
-                        return resolveModuleId(source, false);
-                    }
-                    else {
-                        throw createError(source, `
+async function resolveModuleId(source, skipFetch = false) {
+    let m;
+    if ((m = source.dependency.match(exports.kUriRegex))) {
+        // any + url
+        const [_, scheme, pathname] = m;
+        const cachePath = path.join(cacheDir("deno"), `deps/${scheme}/${pathname}`);
+        if (!(await fileExists(cachePath))) {
+            if (!(await fileExists(cachePath + ".headers.json"))) {
+                if (!skipFetch) {
+                    await fetch_1.fetchModule(source.dependency, cacheDir("deno"));
+                    return resolveModuleId(source, false);
+                }
+                else {
+                    throw createError(source, `
         Cache file was not found in: ${cachePath}. 
         This typically means that you need to run "deno fetch" for the entry file. 
         `);
-                    }
                 }
-                const { redirect_to } = JSON.parse(yield readFileAsync(cachePath + ".headers.json"));
-                return resolveModuleId({
-                    moduleId: ".",
-                    dependency: redirect_to
-                });
             }
-            else {
-                return source.dependency;
-            }
-        }
-        else if (source.moduleId.match(exports.kUriRegex)) {
-            // url + relative
+            const headers = await readFileAsync(cachePath + ".headers.json");
+            const { redirect_to } = JSON.parse(headers);
             return resolveModuleId({
                 moduleId: ".",
-                dependency: url.resolve(source.moduleId, source.dependency)
+                dependency: redirect_to
             });
         }
         else {
-            // relative + relative
-            return joinModuleId(source);
+            return source.dependency;
         }
-    });
+    }
+    else if (source.moduleId.match(exports.kUriRegex)) {
+        // url + relative
+        return resolveModuleId({
+            moduleId: ".",
+            dependency: url.resolve(source.moduleId, source.dependency)
+        });
+    }
+    else {
+        // relative + relative
+        return joinModuleId(source);
+    }
 }
 exports.resolveModuleId = resolveModuleId;
-function traverseDependencyTree(sourceFile, dependencyTree, redirectionMap, opts) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const dependencies = [];
-        let id;
-        id = yield resolveModuleId(sourceFile, opts.skipFetch);
-        redirectionMap.set(joinModuleId(sourceFile), id);
-        if (dependencyTree.has(id)) {
-            return;
+async function traverseDependencyTree(sourceFile, dependencyTree, redirectionMap, opts) {
+    const dependencies = [];
+    let id;
+    id = await resolveModuleId(sourceFile, opts.skipFetch);
+    redirectionMap.set(joinModuleId(sourceFile), id);
+    if (dependencyTree.has(id)) {
+        return;
+    }
+    dependencyTree.set(id, sourceFile);
+    const visit = (node) => {
+        if (ts.isImportDeclaration(node)) {
+            const dependency = node.moduleSpecifier.text;
+            dependencies.push(dependency);
         }
-        dependencyTree.set(id, sourceFile);
-        const visit = (node) => {
-            if (ts.isImportDeclaration(node)) {
-                const dependency = node.moduleSpecifier.text;
-                dependencies.push(dependency);
+        else if (ts.isCallExpression(node) &&
+            node.expression.kind === ts.SyntaxKind.ImportKeyword) {
+            // import("aa").then(v => {})
+            const [module] = node.arguments;
+            if (ts.isStringLiteral(module)) {
+                dependencies.push(module.text);
             }
-            else if (ts.isCallExpression(node) &&
-                node.expression.kind === ts.SyntaxKind.ImportKeyword) {
-                // import("aa").then(v => {})
-                const [module] = node.arguments;
-                if (ts.isStringLiteral(module)) {
+        }
+        else if (ts.isExportDeclaration(node)) {
+            const exportClause = node.exportClause;
+            const module = node.moduleSpecifier;
+            if (exportClause) {
+                if (module) {
+                    // export {a,b} form "bb"
                     dependencies.push(module.text);
-                }
-            }
-            else if (ts.isExportDeclaration(node)) {
-                const exportClause = node.exportClause;
-                const module = node.moduleSpecifier;
-                if (exportClause) {
-                    if (module) {
-                        // export {a,b} form "bb"
-                        dependencies.push(module.text);
-                    }
-                    else {
-                        // export {a,b
-                    }
                 }
                 else {
-                    dependencies.push(module.text);
+                    // export {a,b
                 }
             }
-            ts.forEachChild(node, visit);
-        };
-        const resolvedPath = yield resolveUri(id);
-        const text = yield readFileAsync(resolvedPath);
-        const src = ts.createSourceFile(resolvedPath, text, ts.ScriptTarget.ESNext);
-        ts.forEachChild(src, visit);
-        for (const dependency of dependencies) {
-            yield traverseDependencyTree({ dependency: dependency, moduleId: id }, dependencyTree, redirectionMap, opts);
+            else {
+                dependencies.push(module.text);
+            }
         }
-    });
+        ts.forEachChild(node, visit);
+    };
+    const resolvedPath = await resolveUri(id);
+    const text = await readFileAsync(resolvedPath);
+    const src = ts.createSourceFile(resolvedPath, text, ts.ScriptTarget.ESNext);
+    ts.forEachChild(src, visit);
+    for (const dependency of dependencies) {
+        await traverseDependencyTree({ dependency: dependency, moduleId: id }, dependencyTree, redirectionMap, opts);
+    }
 }
 function joinModuleId(source) {
     if (source.dependency.match(exports.kUriRegex)) {
@@ -160,74 +143,72 @@ exports.joinModuleId = joinModuleId;
 function createError(source, message) {
     return new Error(`moduleId: "${source.moduleId}", dependency: "${source.dependency}": ${message}`);
 }
-function bundle(entry, opts) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const tree = new Map();
-        const redirectionMap = new Map();
-        let canonicalName;
-        if (entry.match(exports.kUriRegex)) {
-            canonicalName = entry;
+async function bundle(entry, opts) {
+    const tree = new Map();
+    const redirectionMap = new Map();
+    let canonicalName;
+    if (entry.match(exports.kUriRegex)) {
+        canonicalName = entry;
+    }
+    else {
+        canonicalName = "./" + path.relative(process.cwd(), entry);
+    }
+    await traverseDependencyTree({
+        dependency: canonicalName,
+        moduleId: "."
+    }, tree, redirectionMap, opts);
+    const printer = ts.createPrinter();
+    let template = await readFileAsync(path.resolve(__dirname, "../template/template.ts"));
+    const resolveModule = (moduleId, dep) => {
+        const redirection = redirectionMap.get(moduleId);
+        if (!redirection) {
+            throw new Error(`${moduleId} not found in redirection map`);
+        }
+        if (dep.match(exports.kUriRegex)) {
+            const ret = redirectionMap.get(dep);
+            if (!ret) {
+                throw new Error(`${dep} not found in redirection map`);
+            }
+            return ret;
         }
         else {
-            canonicalName = "./" + path.relative(process.cwd(), entry);
-        }
-        yield traverseDependencyTree({
-            dependency: canonicalName,
-            moduleId: "."
-        }, tree, redirectionMap, opts);
-        const printer = ts.createPrinter();
-        let template = yield readFileAsync(path.resolve(__dirname, "../template/template.ts"));
-        const resolveModule = (moduleId, dep) => {
-            const redirection = redirectionMap.get(moduleId);
-            if (!redirection) {
-                throw new Error(`${moduleId} not found in redirection map`);
-            }
-            if (dep.match(exports.kUriRegex)) {
-                const ret = redirectionMap.get(dep);
-                if (!ret) {
-                    throw new Error(`${dep} not found in redirection map`);
-                }
-                return ret;
-            }
-            else {
-                return joinModuleId({
-                    moduleId: moduleId,
-                    dependency: dep
-                });
-            }
-        };
-        const modules = [];
-        for (const [moduleId] of tree.entries()) {
-            const transformer = new transform_1.Transformer(moduleId, resolveModule);
-            let text = yield readFileAsync(yield resolveUri(moduleId));
-            if (text.startsWith("#!")) {
-                // disable shell
-                text = "//" + text;
-            }
-            const src = ts.createSourceFile(moduleId, text, ts.ScriptTarget.ESNext);
-            const result = ts.transform(src, transformer.transformers());
-            const transformed = printer.printFile(result
-                .transformed[0]);
-            let body = ts.transpile(transformed, {
-                target: ts.ScriptTarget.ESNext
+            return joinModuleId({
+                moduleId: moduleId,
+                dependency: dep
             });
-            if (transformer.shouldMergeExport) {
-                body = `
+        }
+    };
+    const modules = [];
+    for (const [moduleId] of tree.entries()) {
+        const transformer = new transform_1.Transformer(moduleId, resolveModule);
+        let text = await readFileAsync(await resolveUri(moduleId));
+        if (text.startsWith("#!")) {
+            // disable shell
+            text = "//" + text;
+        }
+        const src = ts.createSourceFile(moduleId, text, ts.ScriptTarget.ESNext);
+        const result = ts.transform(src, transformer.transformers());
+        const transformed = printer.printFile(result
+            .transformed[0]);
+        let body = ts.transpile(transformed, {
+            target: ts.ScriptTarget.ESNext
+        });
+        if (transformer.shouldMergeExport) {
+            body = `
       function __export(m) {
         for (var p in m) if (!tsb.exports.hasOwnProperty(p)) tsb.exports[p] = m[p];
       }
       ${body}
       `;
-            }
-            modules.push(`"${moduleId}": (tsb) => { ${body} }`);
         }
-        const body = `${modules.join(",")}`;
-        template = template.replace("/*{@modules}*/", body);
-        template = template.replace("{@entryId}", yield resolveModuleId({ dependency: canonicalName, moduleId: "." }));
-        const output = ts.transpile(template, {
-            target: ts.ScriptTarget.ESNext
-        });
-        console.log(output);
+        modules.push(`"${moduleId}": (tsb) => { ${body} }`);
+    }
+    const body = `${modules.join(",")}`;
+    template = template.replace("/*{@modules}*/", body);
+    template = template.replace("{@entryId}", await resolveModuleId({ dependency: canonicalName, moduleId: "." }));
+    const output = ts.transpile(template, {
+        target: ts.ScriptTarget.ESNext
     });
+    console.log(output);
 }
 exports.bundle = bundle;
--- dist/fetch.js
+++ dist/fetch.js
@@ -1,12 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const node_fetch_1 = require("node-fetch");
 const url_1 = require("url");
@@ -30,34 +22,32 @@ const kAcceptableMimeTypes = [
     "application/typescript",
     "text/typescript"
 ];
-function fetchModule(url, cacheDirectory) {
-    return __awaiter(this, void 0, void 0, function* () {
-        console.error(`${colors_1.green("Download")} ${url}`);
-        const resp = yield node_fetch_1.default(url, {
-            method: "GET",
-            redirect: "manual"
-        });
-        const dest = urlToCacheFilePath(url, cacheDirectory || cacheDir("tsb"));
-        yield fs.ensureDir(path.dirname(dest));
-        if (400 <= resp.status) {
-            throw new Error(`fetch failed with status code ${resp.status}`);
-        }
-        if (200 <= resp.status && resp.status < 300) {
-            const contentType = resp.headers.get("content-type") || "";
-            if (!kAcceptableMimeTypes.some(v => contentType.startsWith(v))) {
-                throw new Error(`unacceptable content-type for ${url}: ${contentType} `);
-            }
-            yield Promise.all([
-                // TODO: pipe body stream
-                fs.writeFile(dest, yield resp.text()),
-                fs.writeFile(dest + ".headers.json", JSON.stringify({ mime_type: contentType }))
-            ]);
-        }
-        else if (300 <= resp.status) {
-            const redirect_to = resp.headers.get("location");
-            yield fs.writeFile(dest + ".headers.json", JSON.stringify({ redirect_to }));
-            return fetchModule(redirect_to, cacheDirectory);
-        }
+async function fetchModule(url, cacheDirectory) {
+    console.error(`${colors_1.green("Download")} ${url}`);
+    const resp = await node_fetch_1.default(url, {
+        method: "GET",
+        redirect: "manual"
     });
+    const dest = urlToCacheFilePath(url, cacheDirectory || cacheDir("tsb"));
+    await fs.ensureDir(path.dirname(dest));
+    if (400 <= resp.status) {
+        throw new Error(`fetch failed with status code ${resp.status}`);
+    }
+    if (200 <= resp.status && resp.status < 300) {
+        const contentType = resp.headers.get("content-type") || "";
+        if (!kAcceptableMimeTypes.some(v => contentType.startsWith(v))) {
+            throw new Error(`unacceptable content-type for ${url}: ${contentType} `);
+        }
+        await Promise.all([
+            // TODO: pipe body stream
+            fs.writeFile(dest, await resp.text()),
+            fs.writeFile(dest + ".headers.json", JSON.stringify({ mime_type: contentType }))
+        ]);
+    }
+    else if (300 <= resp.status) {
+        const redirect_to = resp.headers.get("location");
+        await fs.writeFile(dest + ".headers.json", JSON.stringify({ redirect_to }));
+        return fetchModule(redirect_to, cacheDirectory);
+    }
 }
 exports.fetchModule = fetchModule;
