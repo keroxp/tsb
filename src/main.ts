84c29b9148478c4517c99eb9a4fc40901977d399
keroxp <kerokerokerop@gmail.com>
2019-09-14 02:59:27 +0900

    ver: v0.7.0

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { bundle } from "./bundle";
 
 caporal
   .name("tsb")
-  .version("0.6.0")
+  .version("0.7.0")
   .argument("file", "entry file path for bundle")
   .option("--skipFetch", "skip fetching remote module recursively")
   .action(action);
